3bf12d1cba1926fc8a07adc9b6f2893136424056
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-03-04 10:20:52 -0800

    add vendor description, update tests and confirm still passing

--- VendorAndOrder.Tests/ModelsTests/VendorsTests.cs
+++ VendorAndOrder.Tests/ModelsTests/VendorsTests.cs
@@ -16,19 +16,19 @@ namespace VendorAndOrder.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Safeway");
+      Vendor newVendor = new Vendor("Safeway", "A Grocery Store");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetVendorName_ReturnsNameOfVendor_String()
     {
-      Vendor newVendor = new Vendor("Safeway");
+      Vendor newVendor = new Vendor("Safeway", "A Grocery Store");
       Assert.AreEqual("Safeway", newVendor.VendorName);
     }
     [TestMethod]
     public void GetVendorID_ReturnsIDOfVendor_Int()
     {
-      Vendor newVendor = new Vendor("Safeway");
+      Vendor newVendor = new Vendor("Safeway", "A Grocery Store");
       Assert.AreEqual(1, newVendor.Id);
     }
     [TestMethod]
@@ -40,7 +40,7 @@ namespace VendorAndOrder.Tests
     [TestMethod]
     public void GetAll_ReturnsVendorListWithOneVendor_List()
     {
-      Vendor newVendor = new Vendor("Safeway");
+      Vendor newVendor = new Vendor("Safeway", "A Grocery Store");
       List<Vendor> vendorList = new List<Vendor> { newVendor };
       CollectionAssert.AreEqual(vendorList, Vendor.GetAll());
     }
--- VendorAndOrder/Models/Vendors.cs
+++ VendorAndOrder/Models/Vendors.cs
@@ -5,13 +5,15 @@ namespace VendorAndOrder.Models
   public class Vendor
   {
     public string VendorName { get; set; }
+    public string VendorDescription { get; set; }
     public int Id { get; }
     private static List<Vendor> _instances = new List<Vendor> { };
     public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription)
     {
       VendorName = vendorName;
+      VendorDescription = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
     }
